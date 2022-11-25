23e2b9af075c9be6681a6322748bdda375d168fe
OyakMasu21 <akzaki17@gmail.com>
2022-11-25 22:24:45 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+let action;
+function saturdayFun(action = 'roller-skate'){
+      //console.log(`This Saturday, I want to ${action}!`);
+      return (`This Saturday, I want to ${action}!`)
+}  
+//saturdayFun(action);
+
+let work;
+function mondayWork(work = 'go to the office'){
+    return `This Monday, I will ${work}.`
+}
+let sym;
+let adj;
+function wrapAdjective(sym='*'){
+    return function wrapper(adj='hard worker'){
+        return `You are ${sym }${ adj}${sym}!`
+    }
+}
+console.log(wrapAdjective("||"));
